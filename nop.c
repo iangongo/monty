c398fb11dc74b26dde1737d05250bda3eb66598e
iangongo <ianmwanyaegongo@gmail.com>
2024-01-18 13:08:06 -0800

    addition of main.c mod.c monty.h mul.c nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- function name nothing
+  *@head: function parameter stack head
+  *@counter: funtion parameter line_number
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
