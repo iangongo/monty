4b2aa92a1406caf9e5775ca1b2d3b83c861d00f3
iangongo <ianmwanyaegongo@gmail.com>
2024-01-18 13:17:04 -0800

    addition of stack.c sub.c swap.c

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - function name rints the top
+ * @head: function parameter stack head
+ * @counter: function parameter line_number
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
